89dcc34ece5235081448ecd21b9c52ed3097f09e
atharsajid <athersajid620@gmail.com>
2022-03-25 09:58:27 -0700

    update

--- api/models.py
+++ api/models.py
@@ -7,8 +7,6 @@ class Products(models.Model):
     description = models.CharField(max_length = 250)
     price=models.IntegerField()
     imagepath=models.CharField(max_length=250,blank=True)
-    date_created = models.DateField(auto_created=True)
-    last_modified = models.DateField(auto_now=True)
 
     def __str___(self):
         return self.title
